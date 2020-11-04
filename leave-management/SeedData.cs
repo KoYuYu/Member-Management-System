feaa4a0fc195000b5d5ecfadf5937c47cd6572e5
Ko Yuyu <yko100@syr.edu>
2020-11-04 01:46:34 -0500

    user auth module

---
+++ leave-management/SeedData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace leave_management
+{
+    public static class SeedData
+    {
+        public static void Seed(UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUsers(userManager);
+        }
+
+        public static void SeedUsers(UserManager<IdentityUser> userManager)
+        {
+            if (userManager.FindByNameAsync("admin").Result == null)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = "admin@localhost.com",
+                    Email = "admin@localhost.com"
+                };
+                var result = userManager.CreateAsync(user, "Ko199312*").Result;
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Administrator").Wait();
+                }
+            }
+        }
+
+        public static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            // role exists or not
+            if (!roleManager.RoleExistsAsync("Administrator").Result)
+            {
+                var role = new IdentityRole
+                {
+                    Name = "Administrator"
+                };
+                var result = roleManager.CreateAsync(role).Result;
+            }
+
+            // role exists or not
+            if (!roleManager.RoleExistsAsync("Employee").Result)
+            {
+                var role = new IdentityRole
+                {
+                    Name = "Employee"
+                };
+                var result = roleManager.CreateAsync(role);
+            }
+        }
+    }
+}
